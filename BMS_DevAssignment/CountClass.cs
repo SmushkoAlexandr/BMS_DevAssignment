b8b22b1de5782946774efa2e43e96296b4f3235e
Smushko Alexandr <destructivizm@gmail.com>
2017-04-07 20:43:03 +0300

    init

---
+++ BMS_DevAssignment/CountClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMS_DevAssignment
+{
+    class CountClass
+    {
+        public static int GetWordsByLength(String file, int characterNumber)
+        {
+            char[] delimiters = new char[] { ' ', '\r', '\n', ',' };
+            var words = file.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            return (words.Where(x => x.Length == characterNumber).ToList()).Count;
+        }
+    }
+}
--- BMS_DevAssignment/MainWindow.xaml.cs
+++ BMS_DevAssignment/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,28 @@ namespace BMS_DevAssignment
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void buttonBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Text Files|*.txt";
+            openFile.Title = "Select a Text File";
+
+            Nullable<bool> result = openFile.ShowDialog();
+
+            if (result == true)
+            {
+                Console.WriteLine(openFile.FileName);
+                lblPath.Content = openFile.FileName;
+                txtMain.Text = File.ReadAllText(openFile.FileName, Encoding.UTF8);
+            }
+        }
+
+        private void buttonCount_Click(object sender, RoutedEventArgs e)
+        {
+            lblNResult.Content = CountClass.GetWordsByLength(txtMain.Text, Int32.Parse(txtNCount.Text));
+
         }
     }
 }
